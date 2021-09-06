a2a6e265107ea3060557c6d7d5e8c5241434f51c
fbjarkes <fbjarkes>
2021-09-06 14:48:44 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "parser": "babel-eslint",
+    "rules": {
+    }
+};
