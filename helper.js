607566eed8890143174552e8678fadf7b094604e
fbjarkes <fbjarkes>
2021-10-31 09:19:02 +0100

    Filter on growth paremeters with Ramda

--- helper.js
+++ helper.js
@@ -1,25 +1,25 @@
 const R = require('ramda');
 
-const filterEpsQQ = (quarter, epsQQ) => { return quarter['eps Q/Q (%)'] >= epsQQ };
 const getLatestYear = (earnings) => earnings[Object.keys(earnings).pop()];
-const firstYear = (earnings) => earnings[Object.keys(earnings).shift()];
 const getLatestQuarter = (quarter) => quarter[Object.keys(quarter).shift()];
-const firstQuarter = (quarter) => quarter[Object.keys(quarter).pop()];
-
 const getLatestEarnings = R.pipe(getLatestYear, getLatestQuarter);
-const isLatestQuarterEpsQQValid = R.pipe(getLatestEarnings, (q, threshold) => filterEpsQQ(q, threshold));
 
 
-const filterByGrowthParameters = (earningsData, {epsQQ = null, epsYY, revQQ, revYY, includePos = true}) => {
-	const earnings = getLatestEarnings(earningsData);
-    return filterEpsQQ(earnings, epsQQ);
-}
-
-const printEarnings = (earningsData) => {
-    // TODO:
-}
+const lastQuarterGrowthFilter = ({epsQQ, revQQ, epsYY, revYY}, earningsData) => {    
+    const lastQuarterEpsQtrByQtrFilter = R.propSatisfies(eps => epsQQ ? eps > epsQQ : true, 'eps Q/Q (%)');      
+    const lastQuarterEpsYearByYearFilter = R.propSatisfies(eps => epsYY ? eps > epsYY : true, 'eps Y/Y (%)');
+    const lastQuarterRevYearByYearFilter = R.propSatisfies(eps => revYY ? eps > revYY : true, 'rev Y/Y (%)');
+    const lastQuarterRevQtrByQtr = R.propSatisfies(eps => revQQ ? eps > revQQ : true, 'rev Q/Q (%)');
+    const filterQuarter = R.compose(
+        R.allPass([
+            lastQuarterEpsQtrByQtrFilter, 
+            lastQuarterEpsYearByYearFilter, 
+            lastQuarterRevYearByYearFilter, 
+            lastQuarterRevQtrByQtr]), 
+        getLatestEarnings);
+    return filterQuarter(earningsData);
+};
 
 module.exports = {
-    filterByGrowthParameters,
-    printEarnings
+    lastQuarterGrowthFilter
 }
--- helper.test.js
+++ helper.test.js
@@ -1,4 +1,4 @@
-const {filterByGrowthParameters} = require('./helper');
+const {lastQuarterGrowthFilter} = require('./helper');
 const {expect} = require('chai');
 
 describe('filterByGrowthParameters', () => {
@@ -19,29 +19,50 @@ describe('filterByGrowthParameters', () => {
         },
         '2021': {
             Q2: {
-                eps: 2,
+                date: 'Jun 2021',
+                eps: 3.61,
+                'eps estimate': '3.06',
+                'eps surprise': '0.55',
+                'eps surprise (%)': '17.97',
                 'eps Q/Q (%)': 9.393939393939398,
+                'rev Q/Q (%)': 11.119602598395106,
+                'eps Y/Y (%)': 100.55555555555556,
+                'rev Y/Y (%)': 55.59122525414659
             },
             Q1: {
-                eps: 1
+                date: 'Mar 2021',
+                eps: 3.3,
+                'eps estimate': '2.34',
+                'eps surprise': '0.96',
+                'eps surprise (%)': '41.03',
+                revenue: 26170000000,
+                'eps Q/Q (%)': -14.948453608247425,
+                'rev Q/Q (%)': -6.768792304951909,
+                'eps Y/Y (%)': 92.98245614035088,
+                'rev Y/Y (%)': 47.51972942502818
             }
         }
     };
 
-    it('should return true for no parameters', () => {
-        expect(filterByGrowthParameters(earningsData, {epsQQ: null})).to.be.true;
-    });
-
     describe('should filter on eps Q/Q (%)', () => {
-        
+        it('should return true for no parameters', () => {
+            expect(lastQuarterGrowthFilter({epsQQ: null}, earningsData)).to.be.true;
+            expect(lastQuarterGrowthFilter({}, earningsData)).to.be.true;
+        });
         it('should be true when above threshold', () => {
-            expect(filterByGrowthParameters(earningsData, {'epsQQ': 9.3})).to.be.true;
+            expect(lastQuarterGrowthFilter({'epsQQ': 9.3}, earningsData)).to.be.true;
         });
         it('should be true when threshold is zero', () => {
-            expect(filterByGrowthParameters(earningsData, {'epsQQ': 0})).to.be.true
+            expect(lastQuarterGrowthFilter({'epsQQ': 0}, earningsData)).to.be.true
         });
         it('should be false when below threshold', () => {
-            expect(filterByGrowthParameters(earningsData, {'epsQQ': 10})).to.be.false
+            expect(lastQuarterGrowthFilter({'epsQQ': 10}, earningsData)).to.be.false
         });
-    });    
+    });
+
+    describe('should filter on EPS and Rev for Q/Q and Y/Y', () => {
+        it('should be true when all parameters are defined and valid', () => {
+            expect(lastQuarterGrowthFilter({epsQQ: 3.0, epsYY: 50, revQQ: 10, revYY: 50}, earningsData));
+        })
+    });
 }); 
