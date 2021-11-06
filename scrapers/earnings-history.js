9838d5b4db29421dcd4d7a622563aa1b5ea26cb0
fbjarkes <fbjarkes>
2021-11-06 06:12:46 +0100

    Cleanup

--- scrapers/earnings-history.js
+++ scrapers/earnings-history.js
@@ -159,28 +159,6 @@ class EarningsHistory {
     }
 }
 
-class SymbolEarningsHistory {
-    
-    #symbol
-    #earnings
-
-    constructor(symbol, earnings) {
-        this.#symbol = symbol;
-        this.#earnings = earnings;
-    }
-    
-    getSymbol() {
-        return this.#symbol;
-    }
-
-    getEarnings() {
-        return this.#earnings;
-    }
-
-    isError() {
-        return Object.entries(this.#symbol).length !== 0;
-    }
-}
 
 module.exports = {
     EarningsHistory
